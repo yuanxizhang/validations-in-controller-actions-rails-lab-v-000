904a159d2369a281d03dc9393cbea3948df2bd2e
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-13 01:47:17 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,7 +7,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-	  @author = Author.new(author_params(:name, :email))
+	  @author = Author.new(author_params)
 	  if @author.valid?
 	  	@author.save
 	  	redirect_to author_path(@author)
